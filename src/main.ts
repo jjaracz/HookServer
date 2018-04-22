f0c5080d84d1710278ef1388bc341965383e0a73
Jan Jaracz <janek@MacBook-Pro-Jan.local>
2018-04-22 16:14:37 +0200

    Add https support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as fs from "fs";
+import * as http from "http";
+import * as https from "https";
+import * as express from "express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const httpsOptions = {
+        key: fs.readFileSync("../secrets/private-key.pem"),
+        cert: fs.readFileSync("../secrets/public-certificate.pem"),
+    };
+
+    const server = express();
+    const app = await NestFactory.create(AppModule, server, {});
+    await app.init();
+
+    http.createServer(server).listen(3000);
+    https.createServer(httpsOptions, server).listen(443);
 }
 bootstrap();
